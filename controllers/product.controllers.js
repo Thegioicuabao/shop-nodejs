d59f4a671b2660f46cd2aa3237b4a8cc821fa7f9
thegioicuabao <vickyvuvo@gmail.com>
2019-10-08 22:47:48 +0700

    Update feature/products-search

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -1,8 +1,18 @@
 var db = require('../db')
 
-var products = db.get('products').value()
 module.exports.index = function(req, res){
+    var products = db.get('products').value()
     res.render('products/index', {
         products: products
+      });
+}
+module.exports.search = function(req, res){
+    var q = req.query.q
+    var products = db.get('products').value()
+    var matchedProduct = products.filter(value => {
+        return value.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    })
+    res.render('products/index', {
+        products: matchedProduct
       })
 }
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,4 +2,5 @@ const express = require('express');
 const controllers = require('../controllers/product.controllers')
 const router = express.Router();
 router.get('/', controllers.index);
+router.get('/search', controllers.search);
 module.exports = router;
