087dcba19ff2f1c37cf14ba51049919d8f3cb2ca
thegioicuabao <vickyvuvo@gmail.com>
2019-10-08 20:59:35 +0700

    Feature/create-user

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 3000
 const userRoutes = require('./routes/user.routes');
 app.set('view engine', 'pug')
 app.set('views', './views')
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => res.render('index', {
     name: 'Buro Shop'
