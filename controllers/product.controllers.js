0c7edcc0a4e631b4377dfbd9665cec7afd84d143
thegioicuabao <vickyvuvo@gmail.com>
2019-10-09 01:51:39 +0700

    Products-pagination

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -1,9 +1,14 @@
 var db = require('../db')
 
 module.exports.index = function(req, res){
-    var products = db.get('products').value()
+    var page = parseInt(req.query.page)|| 1; 
+    var perPage = 8;
+    var start = (page-1)*perPage;
+    var end = start + perPage
+    var products = db.get('products').value().slice(start, end)
     res.render('products/index', {
-        products: products
+        products: products,
+        page: page
       });
 }
 module.exports.search = function(req, res){
