ec5aafe2c40368738c76b67c3dddc8ed08fdb1ed
thegioicuabao <vickyvuvo@gmail.com>
2019-10-10 23:42:01 +0700

    cookie-parser

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const cookieParser = require('cookie-parser')
+
+
 const userRoutes = require('./routes/user.routes');
 const productRoutes = require('./routes/product.routes')
 app.set('view engine', 'pug')
 app.set('views', './views')
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })) 
+app.use(cookieParser())
 
 app.get('/', (req, res) => res.render('index', {
     name: 'Buro Shop'
