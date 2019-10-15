2ebf72dfdb4f17dbb3337a736237fcde5857fc57
thegioicuabao <vickyvuvo@gmail.com>
2019-10-15 21:52:39 +0700

    Update avatar use multer

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -22,6 +22,7 @@ module.exports.create = function(req, res) {
 };
 
 module.exports.postCreate = function(req, res) {
+  req.body.avatar = req.file.path.split('/').slice(1).join('/');
   db.get('users').push(req.body).write();
   res.redirect('/users');
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const controllers = require('../controllers/user.controllers');
 const validateMiddleware = require('../middlewares/validate.middleware');
+var multer  = require('multer')
+var upload = multer({ dest: './public/uploads/' })
 const router = express.Router();
 
 router.get('/', controllers.index);
 router.get('/search', controllers.search);
 router.get('/create', controllers.create);
-router.post('/create', validateMiddleware.postCreate, controllers.postCreate);
+router.post('/create', upload.single('avatar'), validateMiddleware.postCreate, controllers.postCreate);
 module.exports = router;
